120e8142df677f55188ad4d8b035d4fde1ceaaac
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-06-25 14:46:10 +0200

    fixes bugs

--- third-task.js
+++ third-task.js
@@ -7,18 +7,18 @@ const makeTransaction = (transaction) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             const canProcess = Math.random() > 0.3;
-
+            const id = transaction.id;
             if (canProcess) {
-                resolve(transaction.id, delay);
+                resolve({ id, delay });
             }
-            reject(transaction.id);
+            reject(id);
 
         }, delay);
     })
 };
 
-const logSuccess = (id, time) => {
-    console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({ id, delay }) => {
+    console.log(`Transaction ${id} processed in ${delay}ms`);
 };
 
 const logError = id => {
